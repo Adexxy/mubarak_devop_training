80f19a00d11306004b4ff2385d97e06a12ca986c
Mubarak <mubarak05lawal@gmail.com>
2025-08-01 09:29:30 +0100

    new hello.py script

---
+++ hello.py
@@ -0,0 +1,8 @@
+def main():
+    name = "Mubarak"
+    print(f"Hello, {name}! Welcome to open source.")
+    print("This is my first pull request")
+    print("Keep coding. Keep growing.")
+
+if __name__ == "main":
+    main()
